e58d699744cf24854dddbdd8c233c7a5869bba54
Sebastian Galinski <129503983+togezard@users.noreply.github.com>
2026-04-12 00:41:30 +0200

    Create projectData.js with project objects

---
+++ src/model/projectData.js
@@ -0,0 +1,22 @@
+const projects = [
+    {
+        title: "Project One",
+        description: "Description of project one.",
+        links: {
+            repository: "https://github.com/user/project-one",
+            blog: "https://blog.example.com/project-one"
+        },
+        team: "Team Member 1, Team Member 2"
+    },
+    {
+        title: "Project Two",
+        description: "Description of project two.",
+        links: {
+            repository: "https://github.com/user/project-two",
+            blog: "https://blog.example.com/project-two"
+        },
+        team: "Team Member 3, Team Member 4"
+    }
+];
+
+export default projects;
